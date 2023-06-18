7248f1732c327ebd2684e050d1ae5b04353d2f0d
axatmansurovxm <slhddnabdukarimov@gmail.com>
2023-06-18 16:36:13 +0500

    repository(UserRepository, ProductRepository)

---
+++ src/main/java/com/example/shopp/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.shopp.repository;
+
+import com.example.shopp.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,UUID> {
+
+}
