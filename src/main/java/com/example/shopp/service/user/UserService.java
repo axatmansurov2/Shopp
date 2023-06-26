16fc991a739e531b1a80c03b134482bf75e83abd
axatmansurovxm <slhddnabdukarimov@gmail.com>
2023-06-26 15:00:40 +0500

    UserService(some)

--- src/main/java/com/example/shopp/service/user/UserService.java
+++ src/main/java/com/example/shopp/service/user/UserService.java
@@ -1,8 +1,13 @@
 package com.example.shopp.service.user;
 
+import com.example.shopp.dto.UserCreateDto;
 import com.example.shopp.entity.user.User;
 import com.example.shopp.service.BaseService;
 
 public interface UserService extends BaseService<User> {
+
+    User SignIn(String username, String password);
+
+    User SignUp(UserCreateDto userCreateDto);
 
 }
